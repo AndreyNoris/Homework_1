260d3c37efbba1bdd30cb52f50cbb243db55944c
cain <vladganggg@gmail.com>
2021-12-07 16:23:41 +0200

    Update

--- homework_1_0.py
+++ homework_1_0.py
@@ -1,21 +1,21 @@
 """2. Дана матрица целых чисел 10x10.
 Вводится число. Выяснить, какие строки и столбцы матрицы содержат данное число.
 (либо рандом либо чтение из файла (input.txt))
-RANDOM:"""
-
-import random
-
-matrix = [[random.randint(0, 100) for n in range(10)] for m in range(10)]
+READ FILE:"""
+data = []
+with open('input.txt') as f:
+    for line in f:
+        data.append([int(x) for x in line.split()])
 
 rows_list = []
 columns_list = []
 
-for m in matrix:
+for m in data:
     print(m)
 
 var = int(input("Input your number in range(0-100): "))
 
-for i, j in enumerate(matrix):
+for i, j in enumerate(data):
     if var in j:
         rows_list.append(i)
         columns_list.append(j.index(var))
